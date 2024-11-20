76cd3883175beb001637d4917ac8d0b4db486b08
Abdullh1111 <abdullah4474032@gmail.com>
2024-11-20 22:53:23 +0600

    add

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({origin: ["https://online-job-dashboard.vercel.app","https://online-job-frontend.vercel.app"], credentials: true}))
+app.use(cors({origin: ["https://online-job-frontend.vercel.app","https://online-job-dashboard.vercel.app"], credentials: true}))
 app.use(cookieParser())
 
 // routes
