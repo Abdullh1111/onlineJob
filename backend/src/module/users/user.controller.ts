aa381e7be851301b5fbca225a7865053bcc7daa6
Abdullh1111 <abdullah4474032@gmail.com>
2024-11-28 11:23:24 +0600

    c

--- backend/src/module/users/user.controller.ts
+++ backend/src/module/users/user.controller.ts
@@ -16,7 +16,7 @@ const login = catchAsync(async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true, 
-    sameSite: "none" ,
+    sameSite: "strict",
     partitioned: true,
     maxAge: 24 * 60 * 60 * 1000,
   }).status(200).json({
